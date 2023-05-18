18af1e44443397dc19625b15315c4efeb81341a9
Katerina <Nefedova.Katia@yandex.ru>
2023-05-18 19:05:24 +0400

    корректировка users

--- db/scripts/002_ddl_create_users_table.sql
+++ db/scripts/002_ddl_create_users_table.sql
@@ -1,6 +1,6 @@
 create table users (
 	id       serial    primary key,
-	name     text         not null,
-	login    text unique  not null,
-	password text         not null
+	name     varchar         not null,
+	login    varchar unique  not null,
+	password varchar         not null
 )
