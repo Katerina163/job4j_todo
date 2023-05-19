567c863a3f54c6fbe57c0074aeaaea664d373133
Katerina <Nefedova.Katia@yandex.ru>
2023-05-19 10:28:38 +0400

    lombok

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -1,19 +1,36 @@
 package ru.job4j.todo.model;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Task {
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Getter
+    @Setter
     private String name;
+
+    @Getter
+    @Setter
     private String description;
+
+    @Getter
+    @Setter
     private LocalDateTime created;
+
+    @Getter
+    @Setter
     private boolean done;
 }
--- src/main/java/ru/job4j/todo/model/User.java
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -1,17 +1,31 @@
 package ru.job4j.todo.model;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Getter
+    @Setter
     private String name;
+
+    @Getter
+    @Setter
     private String login;
+
+    @Getter
+    @Setter
     private String password;
 }
