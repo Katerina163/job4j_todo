64b16cf27d651e5bd6b72ede0a6fe6fae738c66f
Katerina <Nefedova.Katia@yandex.ru>
2023-05-19 10:14:00 +0400

    config

--- src/main/java/ru/job4j/todo/configuration/StylesConfiguration.java
+++ src/main/java/ru/job4j/todo/configuration/StylesConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
-public class ConfigStyles implements WebMvcConfigurer {
+public class StylesConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
