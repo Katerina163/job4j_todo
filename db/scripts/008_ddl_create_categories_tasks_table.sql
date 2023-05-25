d2bd9ed563ccc63b99f6d8a32780b7c0a5535306
Katerina <Nefedova.Katia@yandex.ru>
2023-05-25 16:25:58 +0400

    sql

--- db/scripts/008_ddl_create_categories_tasks_table.sql
+++ db/scripts/008_ddl_create_categories_tasks_table.sql
@@ -2,5 +2,5 @@ create table categories_tasks (
 	id          serial primary key,
     category_id int    not null references categories(id),
     task_id     int    not null references tasks(id),
-    unique (item_id, user_id)
+    unique (category_id, task_id)
 );
