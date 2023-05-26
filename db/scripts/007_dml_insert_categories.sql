1f8c0a4b8c34b80600b29cf947fbbf4aabd50d9d
Katerina <Nefedova.Katia@yandex.ru>
2023-05-26 11:28:41 +0400

    исправление

--- db/scripts/007_dml_insert_categories.sql
+++ db/scripts/007_dml_insert_categories.sql
@@ -1,3 +1,3 @@
-insert into categories (id, name) values (1, 'no category');
+insert into categories (name) values ('no category');
 insert into categories (name) values ('buy');
 insert into categories (name) values ('meet');
