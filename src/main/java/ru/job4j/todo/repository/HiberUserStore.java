818e8bd8f82e441212b8efee978c2bf130d12424
Katerina <Nefedova.Katia@yandex.ru>
2023-05-29 09:02:04 +0400

    корректировка findById

--- src/main/java/ru/job4j/todo/repository/HiberUserStore.java
+++ src/main/java/ru/job4j/todo/repository/HiberUserStore.java
@@ -1,9 +1,6 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
@@ -14,7 +11,6 @@ import java.util.Optional;
 @AllArgsConstructor
 public class HiberUserStore implements UserStore {
     private final CrudRepository crudRepository;
-    private final SessionFactory sf;
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
@@ -30,18 +26,8 @@ public class HiberUserStore implements UserStore {
 
     @Override
     public Optional<User> findById(int id) {
-        Transaction tr = null;
-        Optional<User> user = Optional.empty();
-        try (Session session = sf.openSession()) {
-            tr = session.beginTransaction();
-            user = Optional.of(session.get(User.class, id));
-            tr.commit();
-        } catch (Exception e) {
-            if (tr != null) {
-                tr.rollback();
-            }
-            e.printStackTrace();
-        }
-        return user;
+        return crudRepository.optional(
+                "from User where id = :fId",
+                User.class, Map.of("fId", id));
     }
 }
